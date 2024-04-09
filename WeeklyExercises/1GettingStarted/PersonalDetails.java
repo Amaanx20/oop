85ea15d22b11829b50e88177553be96434013580
Amaan <146334110+Amaanx20@users.noreply.github.com>
2024-04-09 14:38:22 +0100

    Add files via upload

---
+++ WeeklyExercises/1GettingStarted/PersonalDetails.java
@@ -0,0 +1,8 @@
+public class PersonalDetails {
+    public static void main(String[] args) {
+        System.out.print("Name = Amaan");
+        System.out.print("Address = Leeds Uni");
+        System.out.print("Age = 20");
+        System.out.print("Phone Number = 07**********");
+    }
+}
---
+++ WeeklyExercises/1GettingStarted/helloworld.java
@@ -0,0 +1,7 @@
+public class helloworld {
+//1.Create a new project called “HelloWorld”
+    public static void main(String[] args) {
+        System.out.print("Hello World");
+
+    }
+}
