93613175c953c6599f1a7bc03164aa83231d06d9
amaan <Amaanx20@hotmail.co.uk>
2024-06-26 21:40:34 +0100

    commit1

--- WeeklyExercises/2DataAndExpressions/Fractions.java
+++ WeeklyExercises/2DataAndExpressions/Fractions.java
@@ -1,5 +1,18 @@
+//5) Fraction Write an application that prompts and reads the numerator and denominator of a fraction
+// as integers and then prints the decimal equivalent of the fraction.
+import java.util.Scanner;
 public class Fractions {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input a numerator...");
+        double top = scanner.nextDouble();
+        System.out.print("Input a denominator...");
+        double bottom = scanner.nextDouble();
+        double calc = top / bottom;
+        System.out.println("The decimal version of the fraction is.." + calc);
+        scanner.close();
     }
 }
+
+
+
